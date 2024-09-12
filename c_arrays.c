9432c222ec6b7fa1754dad12900e004091f829c3
Nikhil Anand <nikhilanand1006@gmail.com>
2024-09-12 15:54:47 -0400

    todo generate random number

--- c_arrays.c
+++ c_arrays.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 // We would be working with square matrices of a definite size
 #define N 3
 
+void fillArray(int arr[N][N]) {
+	int i;
+	for (i = 0; i 
+}
+
 int main() {
 	printf("%s - %d\n", "Hello World", N);
+	int a[N][N];
+	int b[N][N];
+	int c[N][N];
+
+	// Fill up the array with random numbers between 1 to 10
 	return 0;
 }
