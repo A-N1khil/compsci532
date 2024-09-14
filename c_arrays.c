238b4d3e40f9d02f5fc2a55c3e697517cfc59076
Nikhil Anand <nikhilanand1006@gmail.com>
2024-09-13 20:08:03 -0400

    generating random numbers

--- c_arrays.c
+++ c_arrays.c
@@ -5,9 +5,18 @@
 // We would be working with square matrices of a definite size
 #define N 3
 
-void fillArray(int arr[N][N]) {
-	int i;
-	for (i = 0; i 
+void fillArray(int arr[N][N], int min, int max) {
+
+	// Setting a seed
+	unsigned int seed = time(0);
+
+	int i, j;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			int randomNum = rand_r(&seed) % (max - min + 1) + min;
+			arr[i][j] = randomNum;
+		}
+	}
 }
 
 int main() {
@@ -17,5 +26,14 @@ int main() {
 	int c[N][N];
 
 	// Fill up the array with random numbers between 1 to 10
+	fillArray(a, 1, 10);
+
+	int i, j;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j< N; j++) {
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
